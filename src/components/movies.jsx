19c107ed96a1692f64ea9b75f7e95c218e3ceb3b
Divyank Dhadi <divyank004@gmail.com>
2021-05-08 05:31:41 +0200

    setup and display movie list

---
+++ src/components/movies.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { getMovies } from "./../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  render() {
+    return (
+      <div>
+        <p>Showing {this.state.movies.length} movies in the database</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Movies;
